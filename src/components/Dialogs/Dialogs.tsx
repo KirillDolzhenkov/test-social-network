791d954c8c5be1c1a75af1b190f4f2b27759f888
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-06-21 23:16:17 +0300

    lesson 22 is done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css"
 
 type DialogsPropsType = {}
@@ -8,8 +9,12 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
     return (
         <div className={styles.dialogsPage}>
             <div className={styles.dialogs}>
-                <div className={styles.dialog}>Dimych</div>
-                <div className={styles.dialog}>Victor</div>
+                <div className={styles.dialog + '' + styles.active}>
+                    <NavLink to="/dialogs/1">Dimych</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to="/dialogs/2">Victor</NavLink>
+                </div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.message}>Hello</div>
