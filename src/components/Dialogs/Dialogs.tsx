10813184bbab476700abf3371af5cbcc7613aed4
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-06-23 23:55:01 +0300

    created BLL at DialogsPage

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,18 +6,41 @@ import { Message } from "./Message/Message";
 type DialogsPropsType = {}
 
 
+type DialogsDataType = {
+    id: number
+    name: string
+}
+type MessagesDataType = {
+    id: number
+    message: string
+}
+
+
+
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
+
+    const DialogsData: Array<DialogsDataType> = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Victor"},
+    ]
+
+    const MessagesData: Array<MessagesDataType> = [
+        {id: 1, message: "Hello"},
+        {id: 2, message: "How are u?"},
+    ]
+
+
     return (
         <div className={styles.dialogsPage}>
 
             <div className={styles.dialogs}>
-                <Dialog name={"Dimych"} id={1}/>
-                <Dialog name={"Victor"} id={2}/>
+                <Dialog name={DialogsData[0].name} id={DialogsData[0].id}/>
+                <Dialog name={DialogsData[1].name} id={DialogsData[1].id}/>
             </div>
 
             <div className={styles.messages}>
-                <Message message={"hello"}/>
-                <Message message={"how are u?"}/>
+                <Message message={MessagesData[0].message} id={MessagesData[0].id}/>
+                <Message message={MessagesData[1].message} id={MessagesData[1].id}/>
             </div>
 
         </div>
--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "../Dialogs.module.css";
 
 type MessagePropsType = {
+    id: number
     message: string
+
 }
 
 const Message: React.FC<MessagePropsType> = (props) => {
