3bc81970b6dc490b13ae39b5c1e290485e359424
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2022-07-03 22:55:00 +0300

    modified navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import style from "./Navbar.module.css";
+import {PATH} from "../../App";
 
 //types:
 type NavbarPropsType = {}
@@ -11,22 +12,22 @@ const Navbar: React.FC<NavbarPropsType> = (props) => {
     return (
         <div className={style.navbar}>
             <div className={style.item}>
-                <NavLink to={"/Profile"} activeClassName={style.active}>My profile</NavLink>
+                <NavLink to={PATH.PROFILE} activeClassName={style.active}>My profile</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to={"/News"} activeClassName={style.active}>News</NavLink>
+                <NavLink to={PATH.NEWS} activeClassName={style.active}>News</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to={"/Dialogs"} activeClassName={style.active}>Messages</NavLink>
+                <NavLink to={PATH.DIALOGS} activeClassName={style.active}>Messages</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to={"/Users"} activeClassName={style.active}>Users</NavLink>
+                <NavLink to={PATH.USERS} activeClassName={style.active}>Users</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to={"/Music"} activeClassName={style.active}>Music</NavLink>
+                <NavLink to={PATH.MUSIC} activeClassName={style.active}>Music</NavLink>
             </div>
             <div className={style.item}>
-                <NavLink to={"/Settings"} activeClassName={style.active}>Settings</NavLink>
+                <NavLink to={PATH.SETTINGS} activeClassName={style.active}>Settings</NavLink>
             </div>
         </div>
     )
